62a7db6589b4cb13d2e1343380bb609a1115a1f5
BeccaO <owolabirebecca99@gmail.com>
2023-02-16 13:33:29 -0800

    Write C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+ 1 /*
+  2  * Created: Becca
+  3  */
+  4
+  5 #include <stdio.h>
+  6
+  7 /**
+  8  * main - Prints "Programming is like building a multilingual
+  9  *                puzzle, followed by a new line.
+ 10  *
+ 11  * Return: Always 0.
+ 12  */
+ 13 int main(void)
+ 14 {
+ 15                 puts("\"Programming is like building a multilingual puzzle");
+ 16
+ 17                         return (0);
+ 18 }
