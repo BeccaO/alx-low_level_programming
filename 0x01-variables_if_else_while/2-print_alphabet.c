a555d0a0e1f8ce3c09b008fd8f4e1170af26d9cc
BeccaO <owolabirebecca99@gmail.com>
2023-02-17 14:08:34 -0800

    Small letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Desription: C program to demonstrate putchar() method
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch++)
+putchar(ch);
+
+return (0);
