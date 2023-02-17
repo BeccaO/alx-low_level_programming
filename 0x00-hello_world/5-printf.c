c08f9c1d2d2a0704f7827b4e24cb67600e18076d
BeccaO <owolabirebecca99@gmail.com>
2023-02-17 00:52:36 -0800

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: prints  with proper grammer, but the outcome is a piece of art,
+ * Description: prints "Programming is like building a multilingual puzzle"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("with proper grammar, but the outcome is a piece of art,\n");
-	printf("with proper grammer, but the outcome is a piece of art")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
