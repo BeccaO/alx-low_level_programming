b2b83de15262514c2303b009eb7a461284d6b534
BeccaO <owolabirebecca99@gmail.com>
2023-02-18 09:16:06 -0800

    Printalpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Description: program that prints the alphabet in lowercase,
+ *and then upper case, followed by a new line.
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch <= 'z';
+	 ch++);
+
+	putchar(ch);
+
+	char ab = 'A';
+
+	for (ab = 'A'; ab <= 'Z';
+	 ab++);
+
+	putchar(ab);
+	"\n";
+
+	return (0);
+}
