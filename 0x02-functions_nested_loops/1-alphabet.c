a7b10166970ad6a72aa5e6d741aa2d9b7879ec40
BeccaO <owolabirebecca99@gmail.com>
2023-02-21 05:47:05 -0800

    Printalpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,10 +5,9 @@
 *print_alphabet - a function to print alphabets
 *Description:a function that prints the alphabet
 *in lowercase followed by a new line
-*
 *Return: Always 0 (Success)
 */
-
+print_alphabet();
 void print_alphabet(void)
 {
 	int b;
