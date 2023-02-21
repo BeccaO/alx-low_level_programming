2a3916779ecfac177a80121377973e9d4f5028f7
BeccaO <owolabirebecca99@gmail.com>
2023-02-21 06:10:44 -0800

    Printalpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,9 @@
 *in lowercase followed by a new line
 *Return: Always 0 (Success)
 */
-void print_alphabet(void);
+void print_alphabet(void)
 {
+	print_alphabet();
 	int b;
 
 	for (b = 'a'; b <= 'z'; b++)
