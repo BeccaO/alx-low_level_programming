864b446db4a12ab705f27bb93c571f9ea77759bc
BeccaO <owolabirebecca99@gmail.com>
2023-02-23 14:13:21 -0800

    Multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*mul - multiplication
+*@a: first Variable
+*@b: second variable
+*Return: Always 0
+*/
+
+int mul(int a, int b)
+{
+	int mul = a * b;
+
+	{
+	return (mul);
+	}
+}
