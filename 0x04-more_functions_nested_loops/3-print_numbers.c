a60cea3392add9e1c1c9a8b7538a582f2202dc1a
BeccaO <owolabirebecca99@gmail.com>
2023-02-23 14:25:30 -0800

    CollaboM

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_numbers - print numbers
+*_putchar: prints data type character
+*Return: Always 0
+*/
+
+void print_numbers(void)
+{
+	for (c >= 48; c <= 57; c++)
+	{
+	putchar (c);
+	}
+	putchar ('\n')
+}
