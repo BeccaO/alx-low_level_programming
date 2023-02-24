7e403cbf1ff70df8b231793d51e3a235a9e6aa46
BeccaO <owolabirebecca99@gmail.com>
2023-02-24 06:37:11 -0800

    Believe

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_most_numbers - prints the numbers,from 0 to 9 and ommit 2 and 4
+*_putchar: function to be used
+*
+*/
+
+void print_most_numbers(void)
+{
+	char b;
+
+	for (b >= 48; b <= 58; b++)
+	{
+	if (b != 50 && b != 52)
+	}
+	putchar(b);
+	}
+	}
+	putchar ('\n');
+}
