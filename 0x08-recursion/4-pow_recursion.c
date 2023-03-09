0fe630c285a7f08a6080a193721aaa6238a6858b
BeccaO <owolabirebecca99@gmail.com>
2023-03-09 11:09:47 -0800

    power of x to y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_pow_recursion - Prints x raised to the power of y recursively
+*@x: variable1
+*@y: variable2
+*Return: x to the power of y
+*/
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+	return (x);
+
+	if (y < 0)
+	return (-1);
+
+	else if (y > 1)
+	return (x * _pow_recursion(x, y - 1));
+	return (1);
+}
