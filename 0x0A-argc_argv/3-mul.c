0e55bdbcf488cbe7db10d16b480cf5ecafa5d807
BeccaO <owolabirebecca99@gmail.com>
2023-03-11 05:25:52 -0800

    program should print the result of the multiplication, followed by a new line

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - Entry point to the program
+*@argc: Arguement count
+*@argv: Arguement vector
+*Return: Error if two arguement are not passed
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, mul = 1;
+
+	if (argc < 2)
+	{
+	printf("Error\n");
+	}
+	else if (argc > 1)
+	{
+	for (i = 1; i < argc; i++)
+	mul = mul * atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
