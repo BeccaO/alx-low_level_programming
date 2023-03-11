ba865a6004ff61b5e71d3a243f87abcb54e72fb2
BeccaO <owolabirebecca99@gmail.com>
2023-03-11 02:42:17 -0800

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+*main - Entry point to the arguement
+*@argc: arguement count
+*@argv: arguement vector
+*Return: Always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
+	return (0);
+}
