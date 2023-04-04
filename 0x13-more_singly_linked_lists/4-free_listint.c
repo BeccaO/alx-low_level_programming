142c50293b5981fc5913afd98bb76ccc4df8650d
BeccaO <owolabirebecca99@gmail.com>
2023-04-04 13:42:22 -0700

    Freelist

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,14 +3,18 @@
 #include <stdio.h>
 #include "lists.h"
 
-
+/**
+*free_listint - functions that frees a list
+*@head: Head of node
+*Return: Nothing
+*/
 
 
 void free_listint(listint_t *head)
 {
 	listint_t *tmp = head;
 	listint_t *next;
-	
+
 	while (tmp != NULL)
 	{
 	next = tmp->next;
