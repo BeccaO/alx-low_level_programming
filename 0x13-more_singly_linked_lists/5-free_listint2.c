004334dcc13e1bc6f46a5c87db444ffb9cc2fca2
BeccaO <owolabirebecca99@gmail.com>
2023-04-05 05:46:08 -0700

    Free2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -14,12 +14,12 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;
 	listint_t *next;
-
+	
 	while (temp != NULL)
 	{
 	next = temp->next;
 	free(temp);
 	temp = next;
-	*head = NULL;
 	}
+	*head = NULL;
 }
